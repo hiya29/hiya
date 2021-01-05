5fd3c8e01244d287216d6d601e44e6dc4de643ba
hiya <hiya.upadhyay@medicapsschool.ac.in>
2021-01-05 14:02:44 +0530

    log added

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint; 
 
 var engine, world;
 var box1, pig1;
@@ -32,9 +33,11 @@ function setup(){
 
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
+    log5 = new Log(870,120,150, -PI/7); 
+    log6 = new Log(230, 180, 80, PI/2); 
 
     bird = new Bird(100,100);
+    Chain1 = new Chain(bird.body, log6.body);
 
 }
 
@@ -58,7 +61,8 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
-    bird.display();
+    log6.display();
+    bird.display(); 
+    Chain1.display();
     platform.display();
 }
